ff52bd6a1a7e38d5ec7fb03c157e0bc1fd764b1a
DESKTOP-G6PGT82\Maciej <maciej.czapiewski3@gmail.com>
2020-11-17 20:57:55 +0100

    Added basic bootstrap

--- src/magazyndjango/inventory/forms.py
+++ src/magazyndjango/inventory/forms.py
@@ -4,8 +4,8 @@ from .models import Inventory
 
 class InventoryAddForm(forms.ModelForm):
 	name			= forms.CharField(label='Nazwa ', widget= forms.TextInput())
-	description		= forms.CharField(label='Opis ', widget= forms.Textarea(attrs={"rows": 30,
-																					"cols": 100
+	description		= forms.CharField(label='Opis ', widget= forms.Textarea(attrs={"rows": 10,
+																					"cols": 50
 																				}))
 	quantity		= forms.DecimalField(label='Ilość ')
 	unit			= forms.CharField(label='Jednostka Magazynowa ', widget= forms.TextInput())
